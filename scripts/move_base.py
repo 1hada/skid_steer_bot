770bc675866f7b9e5770051dcc1afb24b9a66fb2
jonathan sanabria <jonsanria@gmail.com>
2021-03-05 16:05:36 -0500

    adding logic to move_base.py for my Sphere Space Navigation implementation.

--- scripts/move_base.py
+++ scripts/move_base.py
@@ -24,76 +24,95 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
 from geometry_msgs.msg import Pose, Point, Quaternion
-
+from tf.transformations import *
+import numpy as np
+from sphere_space_navigation import SphereSpaceNavigation as SSN
 class GoToPose():
     def __init__(self):
 
-        self.goal_sent = False
+      self.goal_sent = False
 
-	# What to do if shut down (e.g. Ctrl-C or failure)
-	rospy.on_shutdown(self.shutdown)
-	
-	# Tell the action client that we want to spin a thread by default
-	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-	rospy.loginfo("Wait for the action server to come up")
+      # What to do if shut down (e.g. Ctrl-C or failure)
+      rospy.on_shutdown(self.shutdown)
 
-	# Allow up to 5 seconds for the action server to come up
-	self.move_base.wait_for_server(rospy.Duration(5))
+      # Tell the action client that we want to spin a thread by default
+      self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+      rospy.loginfo("Wait for the action server to come up")
+
+      # Allow up to 5 seconds for the action server to come up
+      self.move_base.wait_for_server(rospy.Duration(5))
 
     def goto(self, pos, quat):
 
-        # Send a goal
-        self.goal_sent = True
-	goal = MoveBaseGoal()
-	goal.target_pose.header.frame_id = 'map'
-	goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
-                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+      # Send a goal
+      self.goal_sent = True
+      goal = MoveBaseGoal()
+      goal.target_pose.header.frame_id = 'odom'
+      goal.target_pose.header.stamp = rospy.Time.now()
+      goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat[0], quat[1], quat[2], quat[3]))
 
-	# Start moving
-        self.move_base.send_goal(goal)
+      # Start moving
+      self.move_base.send_goal(goal)
 
-	# Allow TurtleBot up to 60 seconds to complete task
-	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+      # Allow TurtleBot up to 60 seconds to complete task
+      success = self.move_base.wait_for_result(rospy.Duration(60)) 
 
-        state = self.move_base.get_state()
-        result = False
+      state = self.move_base.get_state()
+      result = False
 
-        if success and state == GoalStatus.SUCCEEDED:
-            # We made it!
-            result = True
-        else:
-            self.move_base.cancel_goal()
+      if success and state == GoalStatus.SUCCEEDED:
+          # We made it!
+          result = True
+      else:
+          self.move_base.cancel_goal()
 
-        self.goal_sent = False
-        return result
+      self.goal_sent = False
+      return result
 
     def shutdown(self):
-        if self.goal_sent:
-            self.move_base.cancel_goal()
-        rospy.loginfo("Stop")
-        rospy.sleep(1)
+      if self.goal_sent:
+          self.move_base.cancel_goal()
+      rospy.loginfo("Stop")
+      rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
         rospy.init_node('nav_test', anonymous=False)
         navigator = GoToPose()
 
-        # Customize the following values so they are appropriate for your location
-        position = {'x': 1.22, 'y' : 2.56}
-        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
 
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
+        #################################################
+        ######## SPHERE SPACE NAVIGATION ################
+        #################################################   
+        start = (-4.5,0)
+        goal = (4.5,0)
 
-        if success:
-            rospy.loginfo("Hooray, reached the desired pose")
-        else:
-            rospy.loginfo("The base failed to reach the desired pose")
+        kappa = 1
+        obstacle_li = [((-0.5,-1),2)] 
+        my_SSN = SSN(q_start=start,q_goal=goal,circular_obstacle_li=obstacle_li,kappa=kappa)
+        sphere_space_coordinate_li = my_SSN.get_coordinate_li()
 
-        # Sleep to give the last log messages time to be sent
-        rospy.sleep(1)
+        idx = 0
+        while idx < len(sphere_space_coordinate_li) :
+            xyuv_li = sphere_space_coordinate_li[idx]
+            xy, uv = xyuv_li
+            yaw = np.arctan2(uv[1], uv[0])
+            quaternion = quaternion_from_euler(0, 0, yaw)
+            # Customize the following values so they are appropriate for your location
+            position = {'x': xy[0], 'y' : xy[1]}
 
+            rospy.loginfo("Go to (%s, %s) pose %s", position['x'], position['y'],quaternion)
+            success = navigator.goto(position, quaternion)
+
+            if success:
+                rospy.loginfo("Hooray, reached the desired pose")
+            else:
+                rospy.loginfo("The base failed to reach the desired pose")
+
+            # Sleep to give the last log messages time to be sent
+            rospy.sleep(1)
+            idx += 1
     except rospy.ROSInterruptException:
         rospy.loginfo("Ctrl-C caught. Quitting")
 
